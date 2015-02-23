fda0f39c7cc158073b1f9f7b3720d49fa501ac00
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-02-23 11:09:05 -0500

    // added the boys' entries

--- roster.js
+++ roster.js
@@ -7,6 +7,7 @@
 
 
 // All one dataset but starting with the girls info first.
+// Using the larder format of the entries...
 
 
 var rosterFile = "[/n " + [
@@ -24,4 +25,21 @@ var rosterFile = "[/n " + [
 '{"playerName": "Dorian Adams", "gender": "f", "jerseyNumber": 32, "heightIn": 69, "year": "Freshman", "position": "G"}',
 '{"playerName": "Abbey Titzer", "gender": "f", "jerseyNumber": 34, "heightIn": 73, "year": "Freshman", "position": "F/C"}',
 
-// Now here are the boys	
+// Now here are the boys..
+
+'{"playerName": "Cameron Vines", "gender": "m", "jerseyNumber": 1, "heightIn": 76, "year": "Senior", "position": "G"}',
+'{"playerName": "Davone Daniels", "gender": "m", "jerseyNumber": 2, "heightIn": 74, "year": "Freshman", "position": "G"}',
+'{"playerName": "Josh Riley", "gender": "m", "jerseyNumber": 4, "heightIn": 69, "year": "Freshman", "position": "G"}',
+'{"playerName": "Jonah Coble", "gender": "m", "jerseyNumber": 11, "heightIn": 74, "year": "Freshman", "position": "G"}',
+'{"playerName": "Corey Howard", "gender": "m", "jerseyNumber": 13, "heightIn": 71, "year": "Freshman", "position": "G"}',
+'{"playerName": "Jordan Seele", "gender": "m", "jerseyNumber": 14, "heightIn": 81, "year": "Junior", "position": "F"}',
+'{"playerName": "David Hill", "gender": "m", "jerseyNumber": 21, "heightIn": 73, "year": "Sophomore", "position": "G"}',
+'{"playerName": "Dawson Gore", "gender": "m", "jerseyNumber": 23, "heightIn": 72, "year": "Junior", "position": "G"}',
+'{"playerName": "Nathan Tidwell", "gender": "m", "jerseyNumber": 24, "heightIn": 78, "year": "Junior", "position": "F"}',
+'{"playerName": "Ryan Rebholz", "gender": "m", "jerseyNumber": 30, "heightIn": 79, "year": "Senior", "position": "F"}',
+'{"playerName": "Joe Comerford", "gender": "m", "jerseyNumber": 33, "heightIn": 78, "year": "Sophomore", "position": "F"}',
+'{"playerName": "Danilo Zekovic", "gender": "m", "jerseyNumber": 40, "heightIn": 77, "year": "Junior", "position": "G/F"}',
+'{"playerName": "Aleksa Kojcinovic", "gender": "m", "jerseyNumber": 45, "heightIn": 80, "year": "R. Junior", "position": "F"}'].join(",\n ") + "\n]";
+
+
+
