496a48475cb50b5f036b424fabc2c94e52bfa583
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-02-23 10:37:52 -0500

    // the girls' entries are in

---
+++ roster.js
@@ -0,0 +1,27 @@
+
+
+
+// Katie Harold
+// Roster Dataset
+// Feb. 23, 2015
+
+
+// All one dataset but starting with the girls info first.
+
+
+var rosterFile = "[/n " + [
+'{"playerName": "Katie Harold", "gender": "f", "jerseyNumber": 5, "heightIn": 66, "year": "Senior", "position": "G"}', 
+'{"playerName": "Brittany Morris", "gender": "f", "jerseyNumber": 3, "heightIn": 64, "year": "Sophomore", "position": "G"}',
+'{"playerName": "Kalea Parks", "gender": "f", "jerseyNumber": 11, "heightIn": 72, "year": "R. Sophomore", "position": "F"}',
+'{"playerName": "Deja Horn", "gender": "f", "jerseyNumber": 12, "heightIn": 67, "year": "Freshman", "position": "G"}',
+'{"playerName": "Tityana Miller", "gender": "f", "jerseyNumber": 15, "heightIn": 72, "year": "Senior", "position": "F"}',
+'{"playerName": "Courtney Kvachkoff", "gender": "f", "jerseyNumber": 20, "heightIn": 69, "year": "Junior", "position": "G"}',
+'{"playerName": "Bre Michaels", "gender": "f", "jerseyNumber": 21, "heightIn": 68, "year": "Freshman", "position": "G"}',
+'{"playerName": "Laura Furrer", "gender": "f", "jerseyNumber": 22, "heightIn": 69, "year": "Junior", "position": "G/F"}',
+'{"playerName": "Lauren Davis", "gender": "f", "jerseyNumber": 23, "heightIn": 64, "year": "R. Junior", "position": "G"}',
+'{"playerName": "Cameika Gregory", "gender": "f", "jerseyNumber": 24, "heightIn": 66, "year": "Senior", "position": "G"}',
+'{"playerName": "Rolanda Curington", "gender": "f", "jerseyNumber": 25, "heightIn": 69, "year": "Freshman", "position": "G/F"}',
+'{"playerName": "Dorian Adams", "gender": "f", "jerseyNumber": 32, "heightIn": 69, "year": "Freshman", "position": "G"}',
+'{"playerName": "Abbey Titzer", "gender": "f", "jerseyNumber": 34, "heightIn": 73, "year": "Freshman", "position": "F/C"}',
+
+// Now here are the boys	
